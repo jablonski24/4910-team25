7a0a308690fa8daa1e3097503578b4c6c1cfd5f5
Ben Jablonski <bvjablo@clemson.edu>
2024-02-04 23:21:17 -0500

    dumb truck image

--- truck-app/src/App.js
+++ truck-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import truck_pic from './truck.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={truck_pic} className="truck" alt="truck" />
         <p>
           Team 25 Truck App
         </p>
