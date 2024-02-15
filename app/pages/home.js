0d4fec48b2ea0cf347dacc3bfa47b5099440281f
Ethan Ellis <eellis2@clemson.edu>
2024-02-15 16:22:46 -0500

    Ethan App

--- app/pages/home.js
+++ app/pages/home.js
@@ -12,7 +12,38 @@ export default function Home() {
       </Head>
       <ResponsiveAppBar />
       <main>
-        {/* Your Home page content */}
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <div
+            style={{
+              border: '2px solid #333',
+              borderRadius: '8px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+              display: 'inline-block',
+              overflow: 'hidden',
+            }}
+          >
+            <img
+              src="/truck.png"
+              alt="Truck"
+              style={{
+                maxWidth: '100%',
+                display: 'block',
+              }}
+            />
+          </div>
+          <p
+            style={{
+              fontSize: '2em', // Increase font size
+              fontWeight: 'bold',
+              fontFamily: 'cursive', // Use a different font family
+              marginTop: '20px', // Increase top margin for separation
+              paddingBottom: '20px', // Increase bottom padding
+            }}
+          >
+            You Drive, We Buy
+          </p>
+        </div>
+        {/* Your additional content goes here */}
       </main>
     </>
   );
