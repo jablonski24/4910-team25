ccc7a9c597625ff39983b4bd61614963515fa852
Ethan Ellis <eellis2@clemson.edu>
2024-03-05 14:19:25 -0500

    Ethan Sprint 5

--- app/pages/about.js
+++ app/pages/about.js
@@ -3,9 +3,7 @@ import Head from 'next/head';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { BottomNavigation } from '@mui/material';
 import styles from '@/styles/Home.module.css';
-import { lightBlue } from '@mui/material/colors';
 import ResponsiveAppBar from '../styles/appbar';
 
 export default function About() {
