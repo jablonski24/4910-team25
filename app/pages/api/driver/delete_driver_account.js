c80502376f752d22d776260afd302423a080c1e1
Sean Hermes <s.h.hermes@student.vu.nl>
2024-03-05 16:40:45 -0500

    sprint5

---
+++ app/pages/api/driver/delete_driver_account.js
@@ -0,0 +1,43 @@
+// DELETE driver account
+
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config(); // This loads the .env variables
+
+export default async function handler(req, res) {
+
+    // Database connection configuration
+    const dbConfig = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    };
+
+    console.log(dbConfig);
+
+    try {
+        // Create a connection to the database
+        const connection = await mysql.createConnection(dbConfig);
+
+        const userID = req.query.userID;
+
+        const [result] = await connection.query('DELETE FROM User WHERE userID = ?', [userID]);
+
+        // Close the database connection
+        await connection.end();
+
+       
+        // Send the confirmation as JSON response
+        if (result.affectedRows > 0) {
+            res.status(200).json({ message: 'User deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Database connection or query failed', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
---
+++ app/pages/api/driver/post_send_app_to_sponsor.js
@@ -0,0 +1,43 @@
+// POST send application to Sponsor
+
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config(); // This loads the .env variables
+
+export default async function handler(req, res) {
+
+    // Database connection configuration
+    const dbConfig = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    };
+
+    console.log(dbConfig);
+
+    try {
+        // Create a connection to the database
+        const connection = await mysql.createConnection(dbConfig);
+
+        // get application
+        const {orgID, userID, reason, timestamp} = req.body;
+        
+        // make query
+        const query = 'INSERT INTO Driver_app_audit (orgID, userID, reason, timestamp) VALUES (?,?,?,?)';
+        
+        // send query
+        const [rows] = await connection.query(query,[orgID, userID, reason, timestamp]);
+
+        // Close the database connection
+        await connection.end();
+
+        // Send the data as JSON response
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error('Database connection or query failed', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
