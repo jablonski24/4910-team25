01709bdbf77804a5c5e7df05185a464fe0cb5999
jablonski24 <124194276+jablonski24@users.noreply.github.com>
2024-03-27 19:11:04 -0400

    Update _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,35 +1,48 @@
-// import "@/styles/globals.css";
-// import Amplify from 'aws-amplify';
-// import awsExports from "aws-exports.js";
+import "@/styles/globals.css";
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
-// Amplify.configure(awsExports);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      //  Amazon Cognito User Pool ID
+      userPoolId: 'us-east-1_ujtMueces',
+      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+      userPoolClientId: '100d3kpmsuk6qt752bcqkstbsp',
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+      // OPTIONAL - Set to true to use your identity pool's unauthenticated role when user is not logged in
+      allowGuestAccess: true,
+      // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+      // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+      // signUpVerification Method: 'code', // 'code' | 'link'
+      loginWith: {
+        // OPTIONAL - Hosted UI configuration
+        oauth: {
+          domain: 'https://team25officialsignup.auth.us-east-1.amazoncognito.com/',
+          scopes: [
+            'email',
+            'phone',
+            'openid',
+            'profile',
+          ],
+          redirectSignIn: ['https://main.d2gmf4p0ogiu5s.amplifyapp.com/'],
+          redirectSignOut: ['https://main.d2gmf4p0ogiu5s.amplifyapp.com/'],
+          responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+        }
+      }
+    }
+  }
+});
 
-// function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
+// You can get the current config object
+//const currentConfig = Amplify.getConfig();
+// console.log("Current Config:\n");
+// console.log(Amplify.getConfig());
 
-// export default App;
-
-// pages/_app.js or a specific page like pages/index.js
-import '@/styles/globals.css'; // Ensure this is correctly set up for global styles in Next.js
-import Amplify from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import config from '../path/to/your/amplifyconfiguration.json'; // Adjust the path as necessary
-
-Amplify.configure(config);
-
-function App({ Component, pageProps, signOut, user }) {
-  // You can conditionally render authentication-related components
-  // if you're using this in _app.js and want to control which pages are protected
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-      <Component {...pageProps} />
-    </>
-  );
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default withAuthenticator(App);
 
