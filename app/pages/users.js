6409603a7fda7dffeb36a9714f4b72da506713a7
jablonski24 <124194276+jablonski24@users.noreply.github.com>
2024-03-27 19:26:16 -0400

    Update users.js

--- app/pages/users.js
+++ app/pages/users.js
@@ -1,40 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { Auth } from 'aws-amplify';
+import { getCurrentUser } from 'aws-amplify/auth';
 
 export default function User() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [user, setUser] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const userData = await Auth.currentAuthenticatedUser();
-        setUser(userData);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching user:', error);
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       const userData = await Auth.currentAuthenticatedUser();
+  //       setUser(userData);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.error('Error fetching user:', error);
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchUser();
-  }, []);
+  //   fetchUser();
+  // }, []);
 
-  if (loading) {
-    return <div>Loading user information...</div>;
+  // if (loading) {
+  //   return <div>Loading user information...</div>;
+  // }
+
+  // if (!user) {
+  //   return <div>No user is currently logged in.</div>;
+  // }
+
+  // Display user informatio
+
+  async function currentAuthenticatedUser() {
+  try {
+    const { username, userId } = await getCurrentUser();
+    console.log(`The username: ${username}`);
+    console.log(`The userId: ${userId}`);
+    // console.log(`The signInDetails: ${signInDetails}`);
+  } catch (err) {
+    console.log(err);
   }
-
-  if (!user) {
-    return <div>No user is currently logged in.</div>;
-  }
-
-  // Display user information
+}
   return (
-    <div>
-      <h2>User Profile</h2>
-      <p><strong>Username:</strong> {user.username}</p>
-      {/* Display other user attributes as needed */}
-      <p><strong>Email:</strong> {user.attributes.email}</p>
-    </div>
+    
   );
 };
